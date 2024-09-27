c781ee0ec06046fca6977fa621e8bac3e86fd1c8
Ayesha tahir <ayeshatahir582@gmail.com>
2024-09-27 20:24:59 +0500

    Added purchase

--- Product_stubs.py
+++ Product_stubs.py
@@ -5,10 +5,28 @@ class Product:
       self.price = price
 
   def get_price(self, quantity):
-      pass
+      return 100
 
   def make_purchase(self, quantity):
-      pass
+    try:
+        if quantity > self.amount:
+            raise ValueError("There are not enough items in the stock,sorry you cannot purchase")
+        elif quantity < 0:
+            raise ValueError("The quantity to be purchased should be positive")
+         
+        totalPrice = self.get_price(quantity)
+        print(f"You have successfully purchased the items, Total price is: ${totalPrice:.3f}")
+
+        self.amount= self.amount-quantity  # for showing remaining amount of stock
+        print(f"Remaining stock: {self.amount}")
+        
+    except ValueError as e:
+            print(f"Error: {e}")
+        
+
+
+
+      
 
 # create product object
 # make purchases against different product quantities (make sure to run each test case)
